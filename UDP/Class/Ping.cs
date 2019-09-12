51cf3d34a92512147cf3396dd91d2d4cf8691308
GabrielPuente <gapuente96@gmail.com>
2019-09-12 20:30:11 -0300

    Fase 2 Finalizado
    Fase 2 Finalizado

--- UDP/Class/Ping.cs
+++ UDP/Class/Ping.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,10 +9,13 @@ namespace UDP
     {
         public static void Send(Socket socket, string ip, int port = 60000)
         {
+            string hr = "Heartbeat Request";
+
             var address = IPAddress.Parse(ip);
-            var bytes = Encoding.UTF8.GetBytes("Heartbeat Request");
+            var bytes = Encoding.UTF8.GetBytes(hr);
             var ep = new IPEndPoint(address, port);
             socket.SendTo(bytes, ep);
+            Console.WriteLine($"{ip} \t Sent Message - {hr}");
         }
     }
 }
