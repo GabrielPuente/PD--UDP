c3c06f1105db62542f93f69e8ccc7b7545a69db1
GabrielPuente <gapuente96@gmail.com>
2019-09-19 20:44:46 -0300

    Define leader
    Define leader

---
+++ UDP/Class/User.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UDP.Class
+{
+    public class User
+    {
+        public User() {
+            Alive = true;
+            Retry = 0;
+            Leader = false;
+        }
+
+        public int Priority { get; set; }
+        public string Ip { get; set; }
+        public bool Alive { get; set; }
+        public bool Leader { get; set; }
+        public int Retry { get; set; }
+    }
+}
